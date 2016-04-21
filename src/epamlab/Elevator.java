d154a3ace2825c2ab5fc12154ab8d47ca240cb4a
mirror <mironosk@gmail.com>
2016-04-21 22:10:20 +0300

    all work

--- src/epamlab/Elevator.java
+++ src/epamlab/Elevator.java
@@ -1,5 +1,6 @@
 package epamlab;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.CountDownLatch;
@@ -12,140 +13,147 @@ import org.apache.log4j.Logger;
 
 public class Elevator implements Runnable {
 
-	Logger log = Logger.getLogger(getClass());
-	private Lock lock;
-	private Condition condition;
-	private CountDownLatch countDownLatch;
-	private Floor[] floorsCondition;
+    Logger log = Logger.getLogger(getClass());
+    private Lock lock;
+    private Condition condition;
+    private CountDownLatch countDownLatch;
+    private Floor[] floorsCondition;
 
-	private int capacity;
-	private volatile int currentFloor = 1;
-	private int maxFloor = 10;
-	private boolean directUp = true;
-	private Set<Person> personInElevator;
+    private int capacity;
+    private volatile int currentFloor = 1;
+    private int maxFloor = 10;
+    private boolean directUp = true;
+    private Set<Person> personInElevator;
 
-	private int counter;
-	private int irrivedPerson = 0;
+    private int counter;
+    private int arrivedPerson = 0;
 
-	public Elevator(int capacity, int maxFloor, boolean directUp, Floor[] floorsCondition) {
-		counter = 0;
-		lock = new ReentrantLock();
-		this.condition = lock.newCondition();
-		countDownLatch = new CountDownLatch(0);
-		this.floorsCondition = floorsCondition;
-		this.capacity = capacity;
-		this.maxFloor = maxFloor;
-		this.directUp = directUp;
-		this.personInElevator = new HashSet<>();
-	}
+    public Elevator(int capacity, int maxFloor, boolean directUp, Floor[] floorsCondition) {
+        counter = 0;
+        lock = new ReentrantLock();
+        this.condition = lock.newCondition();
+        countDownLatch = new CountDownLatch(0);
+        this.floorsCondition = floorsCondition;
+        this.capacity = capacity;
+        this.maxFloor = maxFloor;
+        this.directUp = directUp;
+        this.personInElevator = new HashSet<>();
+    }
 
-	public int getCurrentFloor() {
-		return currentFloor;
-	}
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
 
-	public Lock getLock() {
-		return lock;
-	}
+    public Lock getLock() {
+        return lock;
+    }
 
-	public Condition getCondition() {
-		return condition;
-	}
+    public Condition getCondition() {
+        return condition;
+    }
 
-	public CountDownLatch getCountDownLatch() {
-		return countDownLatch;
-	}
+    public CountDownLatch getCountDownLatch() {
+        return countDownLatch;
+    }
 
-	public boolean goToElevator(Person person) {
-		boolean isInElevator = false;
-		try {
-			lock.lock();
-			if (personInElevator.size() < capacity) {
-				personInElevator.add(person);
-				isInElevator = true;
-				log.info("Person " + person + " in elevator");
-			}
-		} finally {
-			lock.unlock();
-		}
-		return isInElevator;
-	}
+    public boolean goToElevator(Person person) {
+        boolean isInElevator = false;
+        try {
+            lock.lock();
+            if (personInElevator.size() < capacity) {
+                personInElevator.add(person);
+                isInElevator = true;
+                log.info("Person " + person + " in elevator");
+            }
+        } finally {
+            lock.unlock();
+        }
+        return isInElevator;
+    }
 
-	public void goOutElevator(Person person) {
+    public void goOutElevator(Person person) {
 
-		try {
-			lock.lock();
-			personInElevator.remove(person);
-			log.info("Person " + person + " is arrive");
-			irrivedPerson++;
-		} finally {
-			lock.unlock();
-		}
-	}
-	
+        try {
+            lock.lock();
+            personInElevator.remove(person);
+            log.info("Person " + person + " is arrive");
+            arrivedPerson++;
+        } finally {
+            lock.unlock();
+        }
+    }
 
-	public void move() {
-		if (currentFloor < maxFloor && directUp) {
-			currentFloor++;
-			if (currentFloor == maxFloor) {
-				directUp = false;
-			}
-		} else if (currentFloor > 1 && !directUp) {
+    public void move() {
+        try {
+            lock.lock();
+            if (currentFloor < maxFloor && directUp) {
+                currentFloor++;
+                if (currentFloor == maxFloor) {
+                    directUp = false;
+                }
+            } else if (currentFloor > 1 && !directUp) {
 
-			currentFloor--;
-			if (currentFloor == 1) {
-				directUp = true;
-			}
-		}
-	}
+                currentFloor--;
+                if (currentFloor == 1) {
+                    directUp = true;
+                }
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
 
-	@Override
-	public void run() {
-		while ((counter < maxFloor * 2) && !personInElevator.isEmpty()) {
-			log.info(currentFloor+"----" + counter);
-			try {
-				goOutFromElevator();
-				goInFromElevator();
-			} catch (InterruptedException e) {
-				log.error(e);
-			}
-			move();
-		}
-		log.info("irrivedPerson--" + irrivedPerson);
+    @Override
+    public void run() {
+        while ((counter < maxFloor * 2) || !personInElevator.isEmpty()) {
+            try {
+                goOutFromElevator();
+                goInFromElevator();
+            } catch (InterruptedException e) {
+                log.error(e);
+            }
+            move();
+        }
+        log.info("arrivedPerson--" + arrivedPerson);
+        log.info("personInElevator " + personInElevator);
+        log.info(Arrays.toString(floorsCondition));
 
-	}
+    }
 
-	private void goInFromElevator() throws InterruptedException {
-		int oldCapacity = personInElevator.size();
-		if (oldCapacity < capacity) {
-			int countNeedPerson = capacity - oldCapacity;
-			int countPersonInFloor = floorsCondition[currentFloor - 1].getTotalCountPersons();
-			if (countPersonInFloor != 0) {
-				if (countPersonInFloor >= countNeedPerson) {
-					countDownLatch = new CountDownLatch(countNeedPerson);
-				} else
-					countDownLatch = new CountDownLatch(countPersonInFloor);
-			}
-			try {
-				floorsCondition[currentFloor - 1].getLock().lock();
-				floorsCondition[currentFloor - 1].getCondition().signalAll();
-			} finally {
-				floorsCondition[currentFloor - 1].getLock().unlock();
-			}
-			countDownLatch.await(1, TimeUnit.SECONDS);
-			if(oldCapacity==personInElevator.size()){
-				counter++;
-			}
-		}
-	}
+    private void goInFromElevator() throws InterruptedException {
+        int oldCapacity = personInElevator.size();
+        if (oldCapacity < capacity) {
+            int countNeedPerson = capacity - oldCapacity;
+            int countPersonInFloor = floorsCondition[currentFloor - 1].getTotalCountPersons();
+            if (countPersonInFloor != 0) {
+                if (countPersonInFloor >= countNeedPerson) {
+                    countDownLatch = new CountDownLatch(countNeedPerson);
+                } else
+                    countDownLatch = new CountDownLatch(countPersonInFloor);
+            }
+            try {
+                floorsCondition[currentFloor - 1].getLock().lock();
+                floorsCondition[currentFloor - 1].getCondition().signalAll();
+            } finally {
+                floorsCondition[currentFloor - 1].getLock().unlock();
+            }
+            countDownLatch.await(1, TimeUnit.SECONDS);
+            if (oldCapacity == personInElevator.size()) {
+                counter++;
+            } else {
+                counter = 0;
+            }
+        }
+    }
 
-	private void goOutFromElevator() throws InterruptedException {
-		countDownLatch = new CountDownLatch(personInElevator.size());
-		try {
-			lock.lock();
-			condition.signalAll();
-		} finally {
-			lock.unlock();
-		}
-		countDownLatch.await(1, TimeUnit.SECONDS);
-	}
+    private void goOutFromElevator() throws InterruptedException {
+        countDownLatch = new CountDownLatch(personInElevator.size());
+        try {
+            lock.lock();
+            condition.signalAll();
+        } finally {
+            lock.unlock();
+        }
+        countDownLatch.await(1, TimeUnit.SECONDS);
+    }
 }
