c7955226c28116a288a756fece95f78bb250be0b
mirror <mironosk@gmail.com>
2016-04-23 09:48:57 +0300

    add test

--- src/test/java/ElevatorTest.java
+++ src/test/java/ElevatorTest.java
@@ -2,8 +2,8 @@ import epamlab.Building;
 import epamlab.Person;
 import epamlab.interfaces.IElevator;
 import epamlab.interfaces.IFloor;
+import org.junit.Before;
 import org.junit.Test;
-
 import epamlab.Elevator;
 import epamlab.Floor;
 
@@ -12,45 +12,45 @@ import java.util.List;
 
 public class ElevatorTest {
 
-	@Test
-	public void test() {
+    private List<Thread> listPersons;
+    private IElevator elevator;
 
-		int countFloors = 20;
-		IFloor[] floors = new IFloor[countFloors];
-		for (int i = 0; i < countFloors; i++) {
-			floors[i] = new Floor();
-		}
+    @Before
+    public void initFloors() {
+        int COUNT_FLOORS = 20;
+        int CAPACITY_ELEVATOR = 10;
+        IFloor[] floors = new IFloor[COUNT_FLOORS];
+        for (int i = 0; i < COUNT_FLOORS; i++) {
+            floors[i] = new Floor();
+        }
+        elevator = new Elevator(CAPACITY_ELEVATOR, COUNT_FLOORS, true, floors);
+        Building building = new Building(floors, elevator);
+        listPersons = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            int needFlor = (int) ((Math.random() * 1000) % 20);
+            int currentFloor = (int) ((Math.random() * 1000) % 20);
+            if (currentFloor == 0) {
+                currentFloor = 1;
+            }
+            if (needFlor == 0) {
+                needFlor = 1;
+            }
+            String name = "" + i;
+            listPersons.add(new Thread(new Person(name, currentFloor, needFlor, building)));
+        }
+    }
 
-		IElevator elevator = new Elevator(8, countFloors, true, floors);
-		Building building = new Building(floors, elevator);
-		List<Thread> listPersons = new ArrayList<>();
-
-		for (int i = 0; i < 100; i++) {
-			int needFlor = (int) ((Math.random() * 1000) % 20);
-			int currentFloor = (int) ((Math.random() * 1000) % 20);
-			if(currentFloor==0){
-				currentFloor=1;
-			}
-			if(needFlor==0){
-				needFlor=1;
-			}
-			String name = "" + i;
-			listPersons.add(new Thread(new Person(name, currentFloor, needFlor, building)));
-		}
-
-		for (Thread thread : listPersons) {
-			thread.start();
-		}
-		Thread elevatorThread = new Thread(elevator);
-		elevatorThread.start();
-		try {
-			elevatorThread.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
-		
-		
-	}
-
+    @Test
+    public void test() {
+        for (Thread thread : listPersons) {
+            thread.start();
+        }
+        Thread elevatorThread = new Thread(elevator);
+        elevatorThread.start();
+        try {
+            elevatorThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
